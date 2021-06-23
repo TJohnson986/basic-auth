ca0b4fad437f3519adce402d9ab47cf4bff3ad1f
Tyler Johnson <tyler@csdconsultants.us>
2021-06-23 14:06:59 -0700

    finished basicAuth middleware

--- middleware/basicAuth.js
+++ middleware/basicAuth.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const bcrypt = require('bcrypt');
+const base64 = require('base-64');
+const { user } = require('../models/index.js');
+
+module.exports = async (req, res, next) => {
+  let basicHeaderParts = req.headers.authorization.split(' ');
+  let encodedString = base64.decode(encodedString);
+  let [username, password] = decodedString.split(':');
+
+  try {
+    const user = await users.findOne({ where: { username: username}});
+    const validUser = await bcrypt.compare(password, user.password);
+    if(valid) {
+      req.user = user;
+      next();
+    } else {
+      throw new Error('Invalid User')
+    }
+  } catch(error) { res.status(403).send('Invalid Login');}
+};
