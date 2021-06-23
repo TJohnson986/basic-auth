7d4f4aa4ca2e0a7c27c320bd0a7e55f55a047de9
Tyler Johnson <tyler@csdconsultants.us>
2021-06-23 14:15:00 -0700

    finished models index.js

--- models/index.js
+++ models/index.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const { Sequelize, DataTypes } = require('sequelize');
+const userSchema = require('./users.js');
+const sequelize = new Sequelize(process.env.DATABASE_URL || 'sqlite:memory:');
+
+const userModel = userSchema(sequelize, DataTypes);
+
+module.exports = {
+  db: sequelize,
+  users: userModel,
+};
