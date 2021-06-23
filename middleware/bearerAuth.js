54f7aabb6063c546085c01a4769514c649ed00d3
Tyler Johnson <tyler@csdconsultants.us>
2021-06-23 14:10:15 -0700

    finished bearerAuth middleware

--- middleware/bearerAuth.js
+++ middleware/bearerAuth.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const { users } = require('../models/index.js');
+
+module.export = async (req, res, next) => {
+  let bearerHeaderParts = req.headers.authorization.split(' ');
+  let token = bearerHeaderParts.pop();
+
+  try {
+    let validUser = await users.authenticateBearer(token);
+    req.user = validUser;
+    next();
+  } catch (e) {
+    console.error('Bearer auth error');
+    next(e)
+  }
+};
